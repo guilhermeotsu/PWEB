1ecb87f0ee715c116f0c9c21b47da41fc9084ed6
Guilherme Otsu <guiotsu@gmail.com>
2020-09-23 22:09:47 -0300

    ...

--- Atividade5/Media.js
+++ Atividade5/Media.js
@@ -1,7 +1,12 @@
 const nome = prompt("Insira seu nome: ")
 
-const n1 = prompt("Insira sua Nota 1: ")
-const n2 = prompt("Insira sua Nota 2: ");
-const n3 = prompt("Insira sua Nota 2: ");
+const n = []
+const max = 3
 
-alert(`${nome} sua média é ${((parseFloat(n1) + parseFloat(n2) + parseFloat(n2)) / 3).toFixed(2)}`)
+for(let i = 0; i < max; i++) {
+  n.push(prompt(`Insira sua Nota ${i + 1}: `))
+}
+
+const avg = n.reduce((avg, values, _, { length }) => (avg + values / length), 0)
+
+alert(`${nome} sua média é ${avg}`)
