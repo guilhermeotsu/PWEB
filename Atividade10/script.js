38440db05b4b0ffa7cf3a0a0b53c34897cd03f46
Guilherme Otsu <guiotsu@gmail.com>
2020-10-28 21:03:33 -0300

    Atividade10

---
+++ Atividade10/script.js
@@ -0,0 +1,21 @@
+const closePath = "./assets/fechada.png"
+const openPath = "./assets/aberta.png"
+const brokenPath = "./assets/quebrada.png"
+
+document.addEventListener("DOMContentLoaded", function() {
+    const $img = document.querySelector("img")
+
+    $img.src = closePath
+
+    $img.addEventListener("mouseover", () => {
+        $img.src = openPath
+    })
+
+    $img.addEventListener("mouseout", () => {
+        $img.src = closePath
+    })
+
+    $img.addEventListener("click", () => {
+        $img.src = brokenPath
+    })
+})
